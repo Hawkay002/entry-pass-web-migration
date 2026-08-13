63ce184088dbf589ee5be2a94d9db8fa72703d2a
Hawkay002 <shovith2002@gmail.com>
2026-08-13 11:38:43 +0530

    Tighter Save button gap (-mt-3)

--- components/admin/settings-form.tsx
+++ components/admin/settings-form.tsx
@@ -326,7 +326,7 @@ export function SettingsForm({ isAdmin = false }: { isAdmin?: boolean }) {
                     </Button>
                   </div>
                 </div>
-                <div className="-mt-2 flex justify-end px-3 pb-2">
+                <div className="-mt-3 flex justify-end px-3 pb-1.5">
                   <Button
                     type="button"
                     size="sm"
